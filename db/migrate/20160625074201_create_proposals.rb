96f1c3490352751505280734e8ce57b435380745
Glen Maddern <glenmaddern@gmail.com>
2016-06-25 17:42:43 +1000

    migrate

--- db/migrate/20160625074201_create_proposals.rb
+++ db/migrate/20160625074201_create_proposals.rb
@@ -1,7 +1,7 @@
 class CreateProposals < ActiveRecord::Migration[5.0]
   def change
     create_table :proposals do |t|
-      t.int :user_id
+      t.integer :user_id
       t.string :title
       t.json :submission
 
