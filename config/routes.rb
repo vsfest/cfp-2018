72ad14b12a46386bb65142527725ff1d0e30ccf1
Glen Maddern <glenmaddern@gmail.com>
2016-06-30 19:31:52 +1000

    fuck users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :proposals
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: redirect("http://cssconf.com.au")
 end
