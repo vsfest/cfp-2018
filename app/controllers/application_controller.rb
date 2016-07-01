c596ea8094cf6596ec728de20902a401b12c1cc6
Glen Maddern <glenmaddern@gmail.com>
2016-07-01 16:49:08 +1000

    change password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
   def check_authentication
-    unless authenticate_or_request_with_http_basic('fuk') { |u,p| u=="staff" && p=="sofknsekreteh"}
+    unless authenticate_or_request_with_http_basic('fuk') { |u,p| u=="team" && p=="they do move in herds"}
       render status: :forbidden
     end
   end
