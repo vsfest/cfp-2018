3047a830ece0f5f652d33d4b7e4aec805a955535
Glen Maddern <glenmaddern@gmail.com>
2016-08-01 14:05:57 +1000

    allowed basic auth for hitting /proposals directly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
   def check_authentication
-    unless (@user = User.find(session[:id]))
+    unless (@user = User.find(session[:id])) || authenticate_or_request_with_http_basic('users') { |u,p| User.authenticate(u,p) }
       render status: :forbidden
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,12 @@ class User
   end
 
   USERS = [
-    new(1, "glen", "$2a$10$CoDVDQO11VrtwtD3Eo/f2evNaeULHpwXeMOOL8WpN7hBqjz8Je0SW"),
-    new(2, "rob", "$2a$04$IfcQpss7kt1Nh1crMSUOC.bTQSjcxwvSwxD.bCvHmV5YsYzIk3/Xu"),
-    new(3, "sharkie", "$2a$04$BIqMhj4hEJfb2gTcByjXbOftQfIBkft8nKlyddu2U07p6LiyoIeAW"),
-    new(4, "xzyfer", "$2a$04$hVKN.wk4l6oTmzOJanWYSea8QR0vO4wBxIawHPzacUhpNKYuAtOlC"),
-    new(5, "fox", "$2a$04$iDwViheGjdJolRmWocJEheIKazndSq.tj1aQgUIvyak4pWiF2IJnW")
+    new(1, 'glen',    '$2a$10$CoDVDQO11VrtwtD3Eo/f2evNaeULHpwXeMOOL8WpN7hBqjz8Je0SW'),
+    new(2, 'rob',     '$2a$04$IfcQpss7kt1Nh1crMSUOC.bTQSjcxwvSwxD.bCvHmV5YsYzIk3/Xu'),
+    new(3, 'sharkie', '$2a$04$BIqMhj4hEJfb2gTcByjXbOftQfIBkft8nKlyddu2U07p6LiyoIeAW'),
+    new(4, 'xzyfer',  '$2a$04$hVKN.wk4l6oTmzOJanWYSea8QR0vO4wBxIawHPzacUhpNKYuAtOlC'),
+    new(5, 'fox',     '$2a$04$iDwViheGjdJolRmWocJEheIKazndSq.tj1aQgUIvyak4pWiF2IJnW'),
+    new(6, 'ben',     '$2a$04$/KLRvhsZ2GwXd8YDHcX9TeRLKHVlHhQkOCTjlb9LJWCJk1uWlqe/6')
   ]
 
   def self.find(id)
