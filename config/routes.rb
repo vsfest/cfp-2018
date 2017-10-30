d6693f2b32ec2635301b4caa649e0defa9a83b43
Glen Maddern <glenmaddern@gmail.com>
2017-10-30 14:03:43 +0100

    started porting the old CFP app to my new react architecture idea

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   resources :rounds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/round1-rejections', to: 'rounds#round1_rejects'
-  get '/round2/:conf', to: 'rounds#round2_candidates'
-  get '/rejections/:conference', to: 'rounds#final_rejections'
+  # get '/round1-rejections', to: 'rounds#round1_rejects'
+  # get '/round2/:conf', to: 'rounds#round2_candidates'
+  # get '/rejections/:conference', to: 'rounds#final_rejections'
 
-  root to: redirect("http://cssconf.com.au")
+  root to: redirect("https://2018.cssconf.com.au")
 end
