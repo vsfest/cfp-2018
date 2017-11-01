b7f197189f2cf69d30bcae9b0b9ca21fc61835fb
Ben Schwarz <ben@germanforblack.com>
2017-11-01 15:15:46 +1100

    Use Base rather than API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
   def check_authentication
