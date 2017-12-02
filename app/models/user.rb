772ec67fc663ee46cd7749efb55c07107a7220d1
Glen Maddern <glenmaddern@gmail.com>
2017-12-02 16:18:35 +1100

    new passwords for some!

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,14 @@ class User
   end
 
   USERS = [
-    new(1, 'glen',    '$2a$10$CoDVDQO11VrtwtD3Eo/f2evNaeULHpwXeMOOL8WpN7hBqjz8Je0SW'),
-    new(2, 'rob',     '$2a$04$IfcQpss7kt1Nh1crMSUOC.bTQSjcxwvSwxD.bCvHmV5YsYzIk3/Xu'),
-    new(3, 'sharkie', '$2a$04$BIqMhj4hEJfb2gTcByjXbOftQfIBkft8nKlyddu2U07p6LiyoIeAW'),
-    new(4, 'xzyfer',  '$2a$04$hVKN.wk4l6oTmzOJanWYSea8QR0vO4wBxIawHPzacUhpNKYuAtOlC'),
-    new(5, 'fox',     '$2a$04$iDwViheGjdJolRmWocJEheIKazndSq.tj1aQgUIvyak4pWiF2IJnW'),
-    new(6, 'ben',     '$2a$04$/KLRvhsZ2GwXd8YDHcX9TeRLKHVlHhQkOCTjlb9LJWCJk1uWlqe/6'),
+    new(1, 'glen',    '$2a$10$/H.s8th.ieVzI23/PMQNQ.DxsnEJVNUxKCJ7aNI9m5cgGH6dD06Oq'),
+    new(2, 'ben',     '$2a$10$RUjEYuG9cYnbvjAI6K5sceUfEbXsDMlxXOQeRm/HRFvdJvReuD/S6'),
+    new(3, 'fox',     '$2a$10$YGW4PdRy5D9jArodr8CW9uVGALY5YR1xU20nXCpAYt647eiHG6cjW'),
+    new(4, 'isakiko', '$2a$10$JtO2lrbOwYBhOuKRZhASeu.8UXVNvJYJSVoGyGychz/AvPkB7a0vC'),
+    new(5, 'xzyfer',  '$2a$10$PFJLm4n4BW/8yP7foehba.Kt8I1vNXl6cDt1NBkysRY5dtc75k/ea'),
+    new(6, 'rob',     '$2a$04$IfcQpss7kt1Nh1crMSUOC.bTQSjcxwvSwxD.bCvHmV5YsYzIk3/Xu'),
     new(7, 'jordan',  '$2a$04$V5z3dX8QdMM.Wxy1d94D9uVGelU5y.JBq9grvdhUcdKHFEkt9stWa'),
-    new(8, 'isakiko', '$2a$04$yx33PKJ.KqAZJbn2WwfLc.p7gixiu1xUBPHmqDAggzyzUMLeI5oGO'),
-    new(9, 'jed',     '$2a$04$uhu7qi1rSPcuyCNzd30yTuVNItYpGOx/mq9O6ctLs9r7RKUYd0zLu'),
-    new(10,'kriesse', '$2a$04$ltFT3vhfY91f/RcEy9h0l.Y0wep8zIAXKE8z9s4qIL.gnsgD3FRCe'),
-    new(11, 'kevin', '$2a$04$9xR/jN9w0u4ZGQ3vTMw2x.pCwsb21fhaq3tBABuR0PsIKHvuXEOdW')
+    new(8, 'sharkie', '$2a$04$BIqMhj4hEJfb2gTcByjXbOftQfIBkft8nKlyddu2U07p6LiyoIeAW'),
   ]
   USERS_BY_ID = USERS.index_by(&:id)
 
